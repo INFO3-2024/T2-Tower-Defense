b2394fbd8b676400dbd9e24dba003c9f9a5bb890
Ramonee <pedrogcorrea3@gmail.com>
2024-09-14 11:06:21 -0300

    Mudanças pra ficar mais top
    obrigado miguel

--- ProjectTestLIBGDX/assets/PlantasEGrama11.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="PlantasEGrama11" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="C:/Users/joaoc/Downloads/Plantas-e-Grama/PlantasEGrama11.png" width="80" height="80"/>
-</tileset>
--- ProjectTestLIBGDX/assets/Terreno-Terra1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Terreno-Terra1" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="C:/Users/joaoc/Downloads/Terra-32px/Terreno-Terra1.png" width="160" height="160"/>
-</tileset>
