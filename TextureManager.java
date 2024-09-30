8fc2fc6d3260fac447a0338de38bf6c51f9575e8
luhvailante1234 <105363292+luhvailante1234@users.noreply.github.com>
2024-09-30 20:49:24 -0300

    Update TextureManager.java

---
+++ TextureManager.java
@@ -0,0 +1,14 @@
+import com.badlogic.gdx.graphics.Texture;
+
+public class TextureManager {
+
+    //testura
+    Texture[] textures = new Texture[4]; 
+
+    //Construtor
+    public TextureManager() {
+
+
+    }
+    
+}
