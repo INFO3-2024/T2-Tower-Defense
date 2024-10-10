c7298d6668aaad06b0f99db90f5774e7d78eef34
Otto-abreu <ottinhoabreu@gmail.com>
2024-10-09 22:29:31 -0300

    Melhoramento e finalização do sistema de rounds
    O código não está exatamente bom, mas não consegui pensar em uma forma melhor para fazer isso. O sistema de rounds agora existe para os 3 mapas

--- ProjectTestLIBGDX/core/src/main/java/Game/game/gameActors/SniperTower.java
+++ ProjectTestLIBGDX/core/src/main/java/Game/game/gameActors/SniperTower.java
@@ -1,14 +1,6 @@
 package Game.game.gameActors;
 
-import java.util.ArrayList;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.utils.Array;
-
 import Game.game.gameAssets.TowerTexture;
 
 public class SniperTower extends Tower {
