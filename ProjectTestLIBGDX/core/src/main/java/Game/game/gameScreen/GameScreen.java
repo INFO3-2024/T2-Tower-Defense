12abc91d4979b4b32cd0464856bd55eda35d1727
Miguel dos Santos Miranda <117553721+Miguelkkkk@users.noreply.github.com>
2024-10-12 11:44:24 -0300

    Menu criado
    Criei o menu principal, o botão de play e de sair já funcionam adequadamente, porém o botão de settings ainda não foi configurado.

--- ProjectTestLIBGDX/core/src/main/java/Game/game/gameScreen/GameScreen.java
+++ ProjectTestLIBGDX/core/src/main/java/Game/game/gameScreen/GameScreen.java
@@ -1,24 +1,26 @@
 package Game.game.gameScreen;
 
+import Game.game.menuStage.MenuStage;
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
 import Game.game.gameStage.GameStage;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class GameScreen implements Screen{
-	
-	
-	private GameStage stage;
-	
+
+
+	private Stage stage;
+
 	public GameScreen(){
-		
-		stage = new GameStage();
+
+		stage = new MenuStage(this);
 	}
-	
+
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-	
-		
+
+
 	}
 
 	@Override
@@ -26,7 +28,7 @@ public class GameScreen implements Screen{
 		// TODO Auto-generated method stub
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		stage.draw();
 		stage.act(delta);
 	}
@@ -34,31 +36,42 @@ public class GameScreen implements Screen{
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-	
+
 	}
 
+
+    public void changeStage(int i) {
+        switch (i){
+            case 1:
+                stage = new GameStage();
+                break;
+            case 2:
+                stage = new MenuStage(this);
+                break;
+        }
+    }
 }
---
+++ ProjectTestLIBGDX/core/src/main/java/Game/game/menuStage/MenuStage.java
@@ -0,0 +1,89 @@
+package Game.game.menuStage;
+
+import Game.game.gameScreen.GameScreen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import Game.game.gameAssets.MapSoundManager;
+
+public class MenuStage extends Stage {
+
+    private static final int STAGE_INDEX = 0;
+    private OrthographicCamera ortho;
+    private BitmapFont font;
+    private SpriteBatch batch;
+    private GameScreen screen;
+    private MapSoundManager soundManager;
+    private Table table;
+
+    public MenuStage(GameScreen screen) {
+        super(new ScreenViewport());
+
+        Gdx.input.setInputProcessor(this);
+
+        ortho = new OrthographicCamera();
+        ortho.setToOrtho(false, 1280, 736);
+
+        soundManager = new MapSoundManager();
+        soundManager.loadSounds();
+
+        table = new Table();
+        table.setFillParent(true);
+        this.addActor(table);
+
+        this.screen = screen;
+
+        createButtons();
+    }
+
+    private void createButtons() {
+        BitmapFont font = new BitmapFont();
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.fontColor = Color.WHITE;
+        textButtonStyle.font.getData().setScale(5f, 5f);
+
+        TextButton playButton = new TextButton("Play", textButtonStyle);
+        TextButton settingsButton = new TextButton("Settings", textButtonStyle);
+        TextButton exitButton = new TextButton("Exit", textButtonStyle);
+
+        playButton.addListener(event -> {
+            if (event instanceof InputEvent && ((InputEvent) event).getType() == InputEvent.Type.touchDown) {
+                System.out.println("Play button clicked");
+                screen.changeStage(1);
+                return true;
+            }
+            return false;
+        });
+
+        settingsButton.addListener(event -> {
+            if (event instanceof InputEvent && ((InputEvent) event).getType() == InputEvent.Type.touchDown) {
+                System.out.println("Settings button clicked");
+                return true;
+            }
+            return false;
+        });
+
+        exitButton.addListener(event -> {
+            if (event instanceof InputEvent && ((InputEvent) event).getType() == InputEvent.Type.touchDown) {
+                Gdx.app.exit();
+                return true;
+          }
+            return false;
+        });
+
+        table.add(playButton).fillX().uniformX().pad(20);
+        table.row().pad(10, 0, 10, 0);
+        table.add(settingsButton).fillX().uniformX().pad(20);
+        table.row();
+        table.add(exitButton).fillX().uniformX().pad(20);
+    }
+}
